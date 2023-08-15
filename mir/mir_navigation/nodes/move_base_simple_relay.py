e92160bc99ff2e95f7ee5e69fa7aaeb78e56ac15
Tobias Recker <Recker@match.uni-hannover.de>
2023-08-15 18:42:27 +0200

    working (somewhat)

--- mir/mir_navigation/nodes/move_base_simple_relay.py
+++ mir/mir_navigation/nodes/move_base_simple_relay.py
@@ -43,12 +43,12 @@ if __name__ == '__main__':
 
     # move_base_flex get_path and move_base action clients
     mbf_mb_ac = actionlib.SimpleActionClient("move_base_flex/move_base", mbf_msgs.MoveBaseAction)
-    mbf_mb_ac.wait_for_server(rospy.Duration(20))
+    mbf_mb_ac.wait_for_server(rospy.Duration(30))
 
     # move_base_flex dynamic reconfigure client
     mbf_drc = Client("move_base_flex", timeout=10)
 
     # move_base simple topic and action server
-    mb_sg = rospy.Subscriber('move_base_simple/goal', PoseStamped, simple_goal_cb)
+    mb_sg = rospy.Subscriber('move_base_simple/goal_match', PoseStamped, simple_goal_cb)
 
     rospy.spin()
